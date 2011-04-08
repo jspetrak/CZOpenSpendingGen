3c77c1370aa8d8b7b15f15ba1bdadc916720d3fa
Josef Petrák <me@jspetrak.name>
2011-04-09 01:23:38 +0200

    Form code valid, basic gen. script version

--- generator.js
+++ generator.js
@@ -1,3 +1,6 @@
 function generate() {
+	var payer = document.forms['form_gen']['t_payer'].value;
+	var title = document.forms['form_gen']['t_title'].value;
 	
+	document.forms['form_gen']['output'].value = "<div typeof=\"payment:Payment\">\n\t<div property=\"payment:Payer\">"+payer+"</div>\n\t<div property=\"dc:title\">"+title+"</div>\n</div>";
 }
